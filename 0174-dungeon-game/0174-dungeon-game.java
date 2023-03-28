16ddc5cdd1a59477e72a407f2eba7fe9435b494a
Ashutosh Kumar <64398311+ashutoshkumar2000@users.noreply.github.com>
2023-03-29 00:10:38 +0530

    Time: 2 ms (59.39%), Space: 42.1 MB (54.66%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,25 +1,27 @@
 class Solution {
-        public int calculateMinimumHP(int[][] A) {
+    public int calculateMinimumHP(int[][] A) {
         int n = A.length;
         int m = A[0].length;
-        int dp[][] = new int[n][m];
+        int dp[] = new int[m];
 
-        dp[n-1][m-1] = Math.max(1, 1 - A[n-1][m-1]);
+        // Initialize the last cell of the dp array
+        dp[m-1] = A[n-1][m-1] < 0 ? Math.abs(A[n-1][m-1])+1 : 1;
 
-        for(int i = n-2; i >= 0; i--){
-            dp[i][m-1] = Math.max(1, dp[i+1][m-1] - A[i][m-1]);
+        // Calculate the last row of the dp array
+        for (int j = m-2; j >= 0; j--) {
+            dp[j] = Math.max(dp[j+1] - A[n-1][j], 1);
         }
 
-        for(int j = m-2; j >= 0; j--){
-            dp[n-1][j] = Math.max(1, dp[n-1][j+1] - A[n-1][j]);
-        }
-
-        for(int i = n-2; i >= 0; i--){
-            for(int j = m-2; j >= 0; j--){
-                dp[i][j] = Math.max(1, Math.min(dp[i+1][j], dp[i][j+1]) - A[i][j]);
+        // Calculate the rest of the dp array row by row
+        for (int i = n-2; i >= 0; i--) {
+            dp[m-1] = Math.max(dp[m-1] - A[i][m-1], 1);
+            for (int j = m-2; j >= 0; j--) {
+                int right = Math.max(dp[j+1] - A[i][j], 1);
+                int down = Math.max(dp[j] - A[i][j], 1);
+                dp[j] = Math.min(right, down);
             }
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 }

86a897745737358b219e4cd2dabf2d9340618c05
Ashutosh Kumar <64398311+ashutoshkumar2000@users.noreply.github.com>
2023-03-29 00:06:04 +0530

    Time: 1 ms (92.21%), Space: 42.1 MB (54.66%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,41 +1,25 @@
 class Solution {
-    public int calculateMinimumHP(int[][] A) {
+        public int calculateMinimumHP(int[][] A) {
         int n = A.length;
         int m = A[0].length;
         int dp[][] = new int[n][m];
-        
-        for(int i = n-1; i >= 0; i--){
-            for(int j = m-1; j >= 0; j--){
-                if(i == n-1 && j == m-1){
-                    dp[n-1][m-1] = A[n-1][m-1] < 0 ? Math.abs(A[n-1][m-1])+1 : 1;
-                }
-                else if(i == n-1){
-                    if(A[i][j] >= dp[i][j+1]){
-                        dp[i][j] = 1;
-                    }
-                    else{
-                        dp[i][j] = dp[i][j+1] - A[i][j];
-                    }
-                }
-                else if(j == m-1){
-                    if(A[i][j] >= dp[i+1][j]){
-                        dp[i][j] = 1;
-                    }
-                    else{
-                        dp[i][j] = dp[i+1][j] - A[i][j];
-                    }
-                }
-                else{
-                    int minDp = Math.min(dp[i+1][j], dp[i][j+1]);
-                    if(A[i][j] > minDp){
-                        dp[i][j] = 1;
-                    }
-                    else{
-                        dp[i][j] = minDp - A[i][j];
-                    }
-                }
+
+        dp[n-1][m-1] = Math.max(1, 1 - A[n-1][m-1]);
+
+        for(int i = n-2; i >= 0; i--){
+            dp[i][m-1] = Math.max(1, dp[i+1][m-1] - A[i][m-1]);
+        }
+
+        for(int j = m-2; j >= 0; j--){
+            dp[n-1][j] = Math.max(1, dp[n-1][j+1] - A[n-1][j]);
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            for(int j = m-2; j >= 0; j--){
+                dp[i][j] = Math.max(1, Math.min(dp[i+1][j], dp[i][j+1]) - A[i][j]);
             }
         }
+
         return dp[0][0];
     }
 }

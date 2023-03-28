9859de4f33f1b755fbb8a58e41ec9f1a78e54ff6
Ashutosh Kumar <64398311+ashutoshkumar2000@users.noreply.github.com>
2023-03-28 15:16:48 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (70.34%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -6,21 +6,28 @@ class Solution {
         return solve(days, costs, 0, dp);
     }
     
-    public int solve(int[] days, int costs[], int idx, int[] dp){
-        // Base Case 
-        if(idx == days.length) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        int weekPassEndDate = days[idx] + 6;
+    public int solve(int[] days, int costs[], int idx, int[] dp){        
+        if(idx == days.length) return 0; // Base Case 
+        
+        if(dp[idx] != -1) return dp[idx]; // Memoization benift
+        
+        int weekPassEndDate = days[idx] + 6; // Calculation of end date for week long Pass
         int i = idx+1;
         while(i < days.length && weekPassEndDate >= days[i]) i++;
-        int MonthPassEndDate = days[idx] + 29;
+        
+        int MonthPassEndDate = days[idx] + 29; // Calculation of end date for month long Pass
         int j = idx+1;
         while(j < days.length && MonthPassEndDate >= days[j]) j++;
-        int dailyPass = solve(days, costs, idx+1, dp) + costs[0];
+        
+        
+        int dailyPass = solve(days, costs, idx+1, dp) + costs[0]; // Creating a Euler Tree
         int weekPass = solve(days, costs, i, dp) + costs[1];
         int monthPass = solve(days, costs, j, dp) + costs[2];
-        int ans = Math.min(dailyPass, Math.min(weekPass, monthPass));
-        dp[idx] = ans;
-        return ans;
+        
+        int ans = Math.min(dailyPass, Math.min(weekPass, monthPass)); // Finding the minimum cost
+        
+        dp[idx] = ans; // Memoization
+        
+        return ans; // Return ans
     }
 }

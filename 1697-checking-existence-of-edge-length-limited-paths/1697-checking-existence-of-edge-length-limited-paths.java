55714944e611b94ccb432f0e1c883bce2b3b9462
Ashutosh Kumar <64398311+ashutoshkumar2000@users.noreply.github.com>
2023-04-30 01:51:38 +0530

    Time: 95 ms (88.52%), Space: 72.8 MB (67.21%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.java
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.java
@@ -1,62 +1,56 @@
-class DSU {
-    private int[] parent;
-    private int[] rank;
-
-    public DSU(int n) {
+class Solution {
+    int parent[];
+    int ht[];
+    public boolean[] distanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries) {
+        for(int i = 0; i < queries.length; i++){
+            queries[i] = new int[]{queries[i][0], queries[i][1], queries[i][2], i};
+        }
+        
+        //init
+        
         parent = new int[n];
-        rank = new int[n];
-        for (int i = 0; i < n; i++) {
+        ht = new int[n];
+        for(int i = 0; i < n; i++){
             parent[i] = i;
         }
-    }
-
-    public int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        return parent[x] = find(parent[x]);
-    }
-
-    public boolean union(int x, int y) {
-        int xset = find(x), yset = find(y);
-        if (xset != yset) {
-            if (rank[xset] < rank[yset]) {
-                parent[xset] = yset;
-            } else {
-                parent[yset] = xset;
-            }
-            if (rank[xset] == rank[yset]) {
-                rank[xset]++;
-            }
-            return true;
-        }
-        return false;
-    }
-}
-
-class Solution {
-    public boolean[] distanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries) {
-        DSU dsu = new DSU(n);
-        for (int i = 0; i < queries.length; i++) {
-            queries[i] = new int[] { queries[i][0], queries[i][1], queries[i][2], i };
-        }
-
+        
         Arrays.sort(queries, (a, b) -> Integer.compare(a[2], b[2]));
         Arrays.sort(edgeList, (a, b) -> Integer.compare(a[2], b[2]));
-
+        
+        boolean ans [] = new boolean[queries.length];
         int i = 0;
-        boolean[] res = new boolean[queries.length];
-        for (int[] q : queries) {
-            while (i < edgeList.length && edgeList[i][2] < q[2]) {
-                dsu.union(edgeList[i][0], edgeList[i][1]);
+        for(int [] query : queries){
+            while(i < edgeList.length && query[2] > edgeList[i][2]){
+                union(edgeList[i][0], edgeList[i][1]);
                 i++;
             }
-
-            if (dsu.find(q[0]) == dsu.find(q[1])) {
-                res[q[3]] = true;
+            
+            if(find(query[0]) == find(query[1])){
+                ans[query[3]] = true;
             }
         }
-
-        return res;
+        return ans;
+    }
+    
+    public void union(int i, int j){
+        int x = find(i);
+        int y = find(j);
+        if(x == y) return;
+        if(ht[x] > ht[y]){
+            parent[y] = x;
+        }
+        else if(ht[y] > ht[x]){
+            parent[x] = y;
+        }
+        else{
+            parent[x] = y;
+            ht[y]++;
+        }
+    }
+    
+    public int find(int x){
+        if(x == parent[x]) return x;
+        parent[x] = find(parent[x]);
+        return parent[x];
     }
 }

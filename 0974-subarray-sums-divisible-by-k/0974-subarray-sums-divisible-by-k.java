41bf08e98dd7e9b585611f3ab60d1a6d46fc7ec8
Ashutosh Kumar <64398311+ashutoshkumar2000@users.noreply.github.com>
2024-06-09 14:36:29 +0530

    Time: 19 ms (76.46%), Space: 51.2 MB (5.13%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,31 +1,30 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int prefix = 0;
         int n = nums.length;
         for(int i = 0; i < n; i++){
             prefix += nums[i];
             prefix = ((prefix%k)+k)%k;
             if(map.containsKey(prefix)){
-                ArrayList<Integer> val = map.get(prefix);
-                val.add(i);
+                int val = map.get(prefix);
+                val++;
                 map.put(prefix, val);
             }
             else {
-                ArrayList<Integer> val = new ArrayList<>();
-                val.add(i);
+                int val = 1;
                 map.put(prefix, val);
             }
         }
         int ans = 0;        
-        for(Map.Entry<Integer, ArrayList<Integer>> elem: map.entrySet()) {
+        for(Map.Entry<Integer, Integer> elem: map.entrySet()) {
             int key = elem.getKey();
-            ArrayList<Integer> val = elem.getValue();
+            int val = elem.getValue();
             if(key == 0) {
-               ans += (val.size() * (val.size()+1))/2;
+               ans += (val * (val+1))/2;
             }
-            else if(val.size() > 1) {
-                ans += ((val.size()-1) * val.size())/2;
+            else if(val > 1) {
+                ans += ((val-1) * val)/2;
             }
         }
         return ans;
